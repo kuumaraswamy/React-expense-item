6acf905e2624098b9303a64dd30eb643463ac661
kuumaraswamy <nkuumaraswamy@gmail.com>
2022-10-08 11:58:50 +0530

    calling expense item

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@
 import React from 'react'
 import ExpenseItem from './components/ExpenseItem'
 const App = () => {
+  const expenses= [
+    {title:"car insurance",amount:250, date: new Date(2022, 2, 28)}
+  ]
+  //  for(let expenses=1;expenses<100;expenses++)
   return (
+    
     <div>
-      <ExpenseItem></ExpenseItem>
-      <h1>Food Rs10</h1>
-      <h1>Petrol Rs100</h1>
-      <h1>Movies Rs200</h1>
+    <h1>Expense Item</h1>
+    <ul>
+      
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} > </ExpenseItem>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} > </ExpenseItem>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} > </ExpenseItem>
+      </ul>
     </div>
   )
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import "./ExpenseItem.css"
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
   return (
-    <div>
-      <h1>Expense Item</h1>
+    <div className="main">
+     <div className='expense-item'></div> 
+     <div>{props.date.toISOString()}</div>
+     <div className='expense-item__description'> </div>
+     <h2> {props.title}</h2>
+     <div className='expense-item__price'>{props.amount}</div>
     </div>
   )
 }
